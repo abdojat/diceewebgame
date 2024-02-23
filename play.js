3032e223de3749978cc7f7cc7fd70045f463e934
abdojat <150303469+abdojat@users.noreply.github.com>
2024-02-23 09:58:55 -0800

    Add files via upload

---
+++ play.js
@@ -0,0 +1,19 @@
+var dice =["","1.png","2.png","3.png","4.png","5.png","6.png"];
+
+var player1 = Math.floor(Math.random()*6)+1;
+var player2 = Math.floor(Math.random()*6)+1;
+if(player1>player2)
+{
+    document.querySelector("h1").textContent = "plyer 1 wins";
+}
+else if (player2>player1)
+{
+    document.querySelector("h1").textContent = "plyer 2 wins";    
+}
+else
+{
+    document.querySelector("h1").textContent = "refresh me";
+}
+
+document.querySelector("img").setAttribute("src","images/"+dice[player1]);
+document.querySelectorAll("img")[1].setAttribute("src","images/"+dice[player2]);
